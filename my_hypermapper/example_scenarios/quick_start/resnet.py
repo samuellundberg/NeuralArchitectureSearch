57669a1cde27a9732b842e148821bf6d163fe538
samuellundberg <samuel.lundberg96@gmail.com>
2020-04-03 14:49:12 +0200

    feat: adding bottleneck block to search space

--- my_hypermapper/example_scenarios/quick_start/resnet.py
+++ my_hypermapper/example_scenarios/quick_start/resnet.py
@@ -68,6 +68,53 @@ def conv1x1(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
+class Bottleneck(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+    expansion = 4
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+
+        norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -170,7 +217,10 @@ class json2ResNet(nn.Module):
         else:
             self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
-        self.fc = nn.Linear(filters * block.expansion * pixels, num_classes)
+        if self.reslays > 0:
+            self.fc = nn.Linear(filters * block.expansion * pixels, num_classes)
+        else:
+            self.fc = nn.Linear(filters * pixels, num_classes)
 
         # What is this??? weight initialization
         for m in self.modules():
@@ -321,21 +371,27 @@ def ResNet_function(X):
     else:
         kernel_size = 3
     """
-    my_net = json2ResNet(BasicBlock, filters, filter_upd, blocks, kernel_size=kernel_size, pool=pool, reduce=reduce)
+    if X['block'] == 0:   # make inline if-statement
+        block = BasicBlock
+    elif X['block'] == 1:
+        block = Bottleneck
+    #block=Bottleneck
+    print(X)
+    my_net = json2ResNet(block, filters, filter_upd, blocks, kernel_size=kernel_size, pool=pool, reduce=reduce)
     # print(my_net)
     # print('parmas: ', count_params(my_net))
     # print('we got a resnet by num lay: ', nbr_layers, 'filters: ', filters, 'blocks: ', blocks)
-    loss = trainer(my_net, train_loader, test_loader, epochs=1)
-    """
+    # loss = trainer(my_net, train_loader, test_loader, epochs=1)
+
     dataiter = iter(train_loader)
     images, labels = dataiter.next()
     outputs = my_net(images)
     import numpy.random as rd
     loss = rd.random()
-    """
-    print('accuracy: ', 100 - loss)
+    #print('accuracy: ', 100 - loss)
     # print('\n')
     return loss
+
 
 def main():
     # Change output path in scenario if you are running something serious
